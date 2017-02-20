96a4da5e66b537fe0950a027a1c3dfb18e08f490
Willi Schinmeyer <willi.schinmeyer@freiheit.com>
2017-02-20 20:22:14 +0100

    add downloading & caching
    also includes cached result for development without repeatedly downloading

--- alert.py
+++ alert.py
@@ -3,9 +3,32 @@
 from bs4 import BeautifulSoup
 import smtplib
 from email.mime.text import MIMEText
+import requests
 
 import local_config
 
+FETCH = False
+CACHE = "cachedresult.html"
+
+def fetchSoup():
+  req = requests.get("https://www.mediamarkt.de/de/search.html?searchProfile=onlineshop&query=nintendo+Switch")
+  assert( req.status_code / 100 == 2 )
+  soup = BeautifulSoup(req.text, "html.parser")
+  return soup
+
+if FETCH:
+  soup = fetchSoup()
+  with open(CACHE, "w") as f:
+    f.write(soup.prettify())
+else:
+  with open(CACHE) as f:
+    soup = BeautifulSoup(f.read(), "html.parser")
+
+#soup.find( attrs = { "data-gtm-prop-list-name": "Search result list" } )
+
+#print(soup)
+
+"""
 msg = MIMEText( "test", "html", "utf-8" )
 msg[ "Subject" ] = "Media Markt Alert"
 msg[ "From" ] = local_config.MAIL_FROM
@@ -14,3 +37,4 @@ msg[ "To" ]  = u", ".join( local_config.MAIL_TO )
 with smtplib.SMTP(local_config.SMTP_SERVER) as s:
   s.login(local_config.SMTP_USER, local_config.SMTP_PASS)
   s.send_message(msg)
+"""
