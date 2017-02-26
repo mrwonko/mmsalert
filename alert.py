b26a8e5c1e89891019a6d3efd8b206c67d6c0178
Willi Schinmeyer <willi.schinmeyer@freiheit.com>
2017-02-26 11:15:51 +0100

    add diffing, improve parsing

--- alert.py
+++ alert.py
@@ -28,6 +28,27 @@ else:
   with open(CACHE) as f:
     soup = BeautifulSoup(f.read(), "html.parser")
 
+class Result:
+  def __init__(self, count, articles):
+    self.count = count
+    self.articles = articles
+  def diff(self, rhs):
+    selfKeys = set(self.articles.keys())
+    rhsKeys = set(rhs.articles.keys())
+    newKeys = rhsKeys.difference(selfKeys)
+    removedKeys = selfKeys.difference(rhsKeys)
+    diffs = []
+    if self.count != rhs.count:
+      diffs.append("number of results changed from {} to {}".format(self.count, rhs.count))
+    if len(newKeys) > 0:
+      diffs.append("new results:\n{}".format(
+          "\n".join(["- " + str(rhs.articles[k]) for k in newKeys])))
+    if len(removedKeys) > 0:
+      diffs.append("removed results:\n{}".format(
+          "\n".join(["- " + str(self.articles[k]) for k in removedKeys])))
+    return diffs
+
+
 class Article:
   def __init__(self, no, name, link):
     self.no = no
@@ -36,23 +57,29 @@ class Article:
   def __str__(self):
     return "{self.no}: {self.name} @ {self.link}".format(self=self)
 
-resultCountText = soup.find(attrs={"class": "cf"}).find("h1").text
-resultCount = int(RESULT_COUNT_RE.search(resultCountText).group(1))
 
-articles = {}
+def parse(soup):
+  resultCountText = soup.select_one("hgroup.cf").find("h1").text
+  resultCount = int(RESULT_COUNT_RE.search(resultCountText).group(1))
 
-resultList = soup.find(attrs={"data-gtm-prop-list-name": "Search result list"})
-for content in resultList.find_all(attrs={"class": "content"}):
-  link = content.find("h2").find("a")
-  title = link.text.strip()
-  articleNoText = content.find(string=ARTICLE_NO_RE)
-  articleNo = int(ARTICLE_NO_RE.search(articleNoText).group(1))
-  articles[articleNo] = Article(
-      articleNo, title, "http://www.mediamarkt.de" + link.get("href"))
+  articles = {}
 
-print("{} results:".format(resultCount))
-for article in articles.values():
-  print(article)
+  resultList = soup.find(attrs={"data-gtm-prop-list-name": "Search result list"})
+  for content in resultList.find_all(class_ = "content"):
+    link = content.find("h2").find("a")
+    title = link.text.strip()
+    articleNoText = content.find(string=ARTICLE_NO_RE)
+    articleNo = int(ARTICLE_NO_RE.search(articleNoText).group(1))
+    articles[articleNo] = Article(
+        articleNo, title, "http://www.mediamarkt.de" + link.get("href"))
+
+  return Result(resultCount, articles)
+
+with open("newresults.html") as f:
+  newSoup = BeautifulSoup(f.read(), "html.parser")
+oldResult = parse(soup)
+newResult = parse(newSoup)
+print("\n".join(oldResult.diff(newResult)))
 
 """
 msg = MIMEText( "test", "html", "utf-8" )
