bd7cd5c4f2674cfbb2250c6d9be1d670f25d32e2
Willi Schinmeyer <willi.schinmeyer@freiheit.com>
2017-02-28 22:17:26 +0100

    style, persist result, do actual check once

--- alert.py
+++ alert.py
@@ -1,93 +1,115 @@
 #!/usr/bin/env python3
 
-from bs4 import BeautifulSoup
 import smtplib
 from email.mime.text import MIMEText
 import requests
 import re
+import pickle
+
+from bs4 import BeautifulSoup
 
 import local_config
 
 FETCH = False
 CACHE = "cachedresult.html"
+RESULT_CACHE = "result.pickle"
 
-RESULT_COUNT_RE = re.compile("(\d+)\s+Ergebnisse")
-ARTICLE_NO_RE = re.compile("Artikelnummer\s+(\d+)")
+RESULT_COUNT_RE = re.compile(r"(\d+)\s+Ergebnisse")
+ARTICLE_NO_RE = re.compile(r"Artikelnummer\s+(\d+)")
 
-def fetchSoup():
-  req = requests.get("https://www.mediamarkt.de/de/search.html?searchProfile=onlineshop&query=nintendo+Switch")
-  assert( req.status_code / 100 == 2 )
-  soup = BeautifulSoup(req.text, "html.parser")
-  return soup
+def fetch_soup():
+    req = requests.get(
+        "https://www.mediamarkt.de/de/search.html?searchProfile=onlineshop&query=nintendo+Switch")
+    assert req.status_code / 100 == 2
+    soup = BeautifulSoup(req.text, "html.parser")
+    return soup
 
-if FETCH:
-  soup = fetchSoup()
-  with open(CACHE, "w") as f:
-    f.write(soup.prettify())
-else:
-  with open(CACHE) as f:
-    soup = BeautifulSoup(f.read(), "html.parser")
-
-class Result:
-  def __init__(self, count, articles):
-    self.count = count
-    self.articles = articles
-  def diff(self, rhs):
-    selfKeys = set(self.articles.keys())
-    rhsKeys = set(rhs.articles.keys())
-    newKeys = rhsKeys.difference(selfKeys)
-    removedKeys = selfKeys.difference(rhsKeys)
-    diffs = []
-    if self.count != rhs.count:
-      diffs.append("number of results changed from {} to {}".format(self.count, rhs.count))
-    if len(newKeys) > 0:
-      diffs.append("new results:\n{}".format(
-          "\n".join(["- " + str(rhs.articles[k]) for k in newKeys])))
-    if len(removedKeys) > 0:
-      diffs.append("removed results:\n{}".format(
-          "\n".join(["- " + str(self.articles[k]) for k in removedKeys])))
-    return diffs
+class Result(object):
+    def __init__(self, count, articles):
+        self.count = count
+        self.articles = articles
+    def diff(self, rhs):
+        self_keys = set(self.articles.keys())
+        rhs_keys = set(rhs.articles.keys())
+        new_keys = rhs_keys.difference(self_keys)
+        removed_keys = self_keys.difference(rhs_keys)
+        diffs = []
+        if self.count != rhs.count:
+            diffs.append("number of results changed from {} to {}".format(self.count, rhs.count))
+        if len(new_keys) > 0:
+            diffs.append("new results:\n{}".format(
+                "\n".join(["- " + str(rhs.articles[k]) for k in new_keys])))
+        if len(removed_keys) > 0:
+            diffs.append("removed results:\n{}".format(
+                "\n".join(["- " + str(self.articles[k]) for k in removed_keys])))
+        return diffs
 
 
-class Article:
-  def __init__(self, no, name, link):
-    self.no = no
-    self.name = name
-    self.link = link
-  def __str__(self):
-    return "{self.no}: {self.name} @ {self.link}".format(self=self)
+def store(obj):
+    with open(RESULT_CACHE, "wb") as f:
+        pickle.dump(obj, f)
+
+
+def load():
+    try:
+        with open(RESULT_CACHE, "rb") as f:
+            return pickle.load(f)
+    except FileNotFoundError:
+        return Result(0, {})
+
+
+class Article(object):
+    def __init__(self, no, name, link):
+        self.num = no
+        self.name = name
+        self.link = link
+    def __str__(self):
+        return "{self.num}: <a href=\"{self.link}\">{self.name}</a>".format(self=self)
 
 
 def parse(soup):
-  resultCountText = soup.select_one("hgroup.cf").find("h1").text
-  resultCount = int(RESULT_COUNT_RE.search(resultCountText).group(1))
+    result_count_text = soup.select_one("hgroup.cf").find("h1").text
+    result_count = int(RESULT_COUNT_RE.search(result_count_text).group(1))
 
-  articles = {}
+    articles = {}
 
-  resultList = soup.find(attrs={"data-gtm-prop-list-name": "Search result list"})
-  for content in resultList.find_all(class_ = "content"):
-    link = content.find("h2").find("a")
-    title = link.text.strip()
-    articleNoText = content.find(string=ARTICLE_NO_RE)
-    articleNo = int(ARTICLE_NO_RE.search(articleNoText).group(1))
-    articles[articleNo] = Article(
-        articleNo, title, "http://www.mediamarkt.de" + link.get("href"))
+    result_list = soup.find(attrs={"data-gtm-prop-list-name": "Search result list"})
+    for content in result_list.find_all(class_="content"):
+        link = content.find("h2").find("a")
+        title = link.text.strip()
+        article_num_text = content.find(string=ARTICLE_NO_RE)
+        article_num = int(ARTICLE_NO_RE.search(article_num_text).group(1))
+        articles[article_num] = Article(
+            article_num, title, "http://www.mediamarkt.de" + link.get("href"))
 
-  return Result(resultCount, articles)
+    return Result(result_count, articles)
 
-with open("newresults.html") as f:
-  newSoup = BeautifulSoup(f.read(), "html.parser")
-oldResult = parse(soup)
-newResult = parse(newSoup)
-print("\n".join(oldResult.diff(newResult)))
 
-"""
-msg = MIMEText( "test", "html", "utf-8" )
-msg[ "Subject" ] = "Media Markt Alert"
-msg[ "From" ] = local_config.MAIL_FROM
-msg[ "To" ]  = u", ".join( local_config.MAIL_TO )
+def alert(diff):
+    diff_str = "\n".join(diff)
+    print(diff_str)
+    msg = MIMEText(diff_str.replace("\n", "<br/>\n"), "html", "utf-8")
+    msg["Subject"] = "Media Markt Alert"
+    msg["From"] = local_config.MAIL_FROM
+    msg["To"] = u", ".join(local_config.MAIL_TO)
 
-with smtplib.SMTP(local_config.SMTP_SERVER) as s:
-  s.login(local_config.SMTP_USER, local_config.SMTP_PASS)
-  s.send_message(msg)
-"""
+    with smtplib.SMTP(local_config.SMTP_SERVER) as s:
+        s.login(local_config.SMTP_USER, local_config.SMTP_PASS)
+        s.send_message(msg)
+
+
+def main():
+    old_result = load()
+    if not isinstance(old_result, Result):
+        print("we're in a pickle, old_result is {}".format(old_result))
+        old_result = Result(0, {})
+    soup = fetch_soup()
+    result = parse(soup)
+    diff = old_result.diff(result)
+    print(len(diff))
+    if len(diff) > 0:
+        store(result)
+        alert(diff)
+
+
+main()
